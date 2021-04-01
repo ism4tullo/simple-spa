7a99e9af397567553eeb8618ff105fd26cb85916
ism4tullo <reven13022002@gmail.com>
2021-04-01 10:43:49 +0500

    dodelal

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const URL = 'https://api.thecatapi.com/v1/images/search'
-
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const http = new XMLHttpRequest()
+const ReqUrl = 'https://api.thecatapi.com/v1/images/search';
+
+let answer
+let imageUrl
+
+function changeAttribute() {
+  http.open('GET', ReqUrl)
+  http.send()
+  http.onload = () => {
+  answer = http.response
+  imageUrl = JSON.parse(answer)["0"].url
+}
+  setTimeout(() => {
+  let img = document.getElementById('img')
+  img.setAttribute("src", imageUrl)
+  }, 500);
+  
+}
